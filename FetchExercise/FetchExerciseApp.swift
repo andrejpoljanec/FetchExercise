96917a80590df1964f3d156e8e6dabda7ef793a2
Andrej Poljanec <andrej.poljanec@gmail.com>
2025-02-12 19:52:30 -0600

    Initial Commit

---
+++ FetchExercise/FetchExerciseApp.swift
@@ -0,0 +1,32 @@
+//
+//  FetchExerciseApp.swift
+//  FetchExercise
+//
+//  Created by Andrej Poljanec on 2/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FetchExerciseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FetchExerciseTests/FetchExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchExerciseTests.swift
+//  FetchExerciseTests
+//
+//  Created by Andrej Poljanec on 2/12/25.
+//
+
+import Testing
+@testable import FetchExercise
+
+struct FetchExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
